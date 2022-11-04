3d906caf483ad04eb8b25197af74664e65a899f9
Achufam24 <achuulimagbama@gmail.com>
2022-11-04 11:20:21 +0100

    second task commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const PORT = 4000
 
 const app = express();
 var cors = require('cors');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(cors({credentials:true, origin:true}));
 
@@ -15,6 +19,34 @@ app.get('/', (req,res) => {
         "bio": "Achu Agbama is a Software Developer at 525 system, a software developemnt company. He has over 2 years experience in frontend and mobile develoment using technologies such as vue and flutter. He is currently learning node js and express. "
     })
 })
+app.post('/', (req,res) => {
+
+    var operation_type = String(req.body.operation_type)
+    var x = Number(req.body.x);
+    var y = Number(req.body.y);
+  
+    if (operation_type === "addition") {
+      var result = x + y;
+    }
+    if (operation_type === "subtraction") {
+      var result = x - y;
+    }
+    if (operation_type === "multiplication") {
+      var result = x * y;
+    }
+    else if (operation_type === "division") {
+      var result = x / y;
+    }
+    
+      
+  
+      res.send({
+          "slackUsername": "Achufam24",
+          "operation_type": operation_type,
+          "result":result
+      })
+  //   res.send("Addition - " + result);
+  })
 
 app.listen(PORT, () => {
     console.log('listening for requests at PORT', PORT);
